d56ce2685c860dc416e8227446bc4089239b97cd
LinhNV <nvlk12haui@gmail.com>
2021-02-05 16:19:15 +0700

    Refactor demo resttemplate

--- src/main/java/com/example/demoresttemplate/DemoResttemplateApplication.java
+++ src/main/java/com/example/demoresttemplate/DemoResttemplateApplication.java
@@ -1,9 +1,12 @@
 package com.example.demoresttemplate;
 
+import com.example.demoresttemplate.configs.PropertiesConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PropertiesConfig.class)
 public class DemoResttemplateApplication {
 
     public static void main(String[] args) {
